32d962ee7501f8f68c8f90441261b186e435cd44
kevinziolko <72788970+kevinziolko@users.noreply.github.com>
2021-05-02 19:53:16 +0200

    Update Account.java

--- bank/Account.java
+++ bank/Account.java
@@ -1,18 +1,39 @@
 package bank;
 
-class Account {
+        class Account {
 
-    // Attributes
-    // TODO
+            // Attributes
 
-    // Constructor
-    // TODO
+            private String name;
+            private int balance;
+            private int decouvert;
+            private boolean block;
 
-    // Methods
-    // TODO
+            // Constructor
 
-    public String toString() {
-        // TODO
-        return "";
-    }
-}
+            public void setName(String name) {
+                this.name = name;
+            }
+
+            public void setBalance(int balance) { this.balance = balance; }
+
+            public void setDecouvert(int decouvert) { this.decouvert = decouvert; }
+
+            public void setBlock(boolean block) { this.block = block; }
+
+            // Methods
+
+            public String getName() { return name; }
+
+            public int getBalance() { return balance; }
+
+            public int getDecouvert() { return decouvert; }
+
+            public boolean isBlock() { return block; }
+
+            public String toString() {
+                String status = "";
+                status = status + this.name + " | " + this.balance + " | " + this.decouvert + " | " + this.block + "\n";
+                return status;
+            }
+        }
